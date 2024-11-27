50cdbe667e81f73f980d710b1567647ca4a24c07
Vinay Patel <vinayspatel0@gmail.com>
2024-11-27 11:03:06 +0000

    feat: initial commit

---
+++ src/main/java/com/northcoders/drinksapi/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.northcoders.drinksapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+    @GetMapping(value ="/")
+    public String home(){
+        return "Welcome to the Drinks API!";
+    }
+
+}
---
+++ src/test/java/com/northcoders/drinksapi/DrinksapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.drinksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinksapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
