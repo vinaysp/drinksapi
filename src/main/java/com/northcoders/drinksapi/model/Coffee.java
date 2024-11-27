59bc82271be48f165604a6d4cada0fab8d174a70
Vinay Patel <vinayspatel0@gmail.com>
2024-11-27 12:31:28 +0000

    feat: create Coffee class

--- src/main/java/com/northcoders/drinksapi/model/Coffee.java
+++ src/main/java/com/northcoders/drinksapi/model/Coffee.java
@@ -1,4 +1,20 @@
 package com.northcoders.drinksapi.model;
 
-public record Coffee() {
+public class Coffee{
+
+    long id;
+    String name;
+
+    public Coffee(long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
