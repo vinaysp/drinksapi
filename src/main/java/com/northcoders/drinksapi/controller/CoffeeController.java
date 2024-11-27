360fee06eb29ce27f1f5ee525e443b5d56219a32
Vinay Patel <vinayspatel0@gmail.com>
2024-11-27 12:30:58 +0000

    feat: create CoffeeController class

--- src/main/java/com/northcoders/drinksapi/controller/CoffeeController.java
+++ src/main/java/com/northcoders/drinksapi/controller/CoffeeController.java
@@ -1,5 +1,9 @@
 package com.northcoders.drinksapi.controller;
 
+import com.northcoders.drinksapi.model.Coffee;
+import com.northcoders.drinksapi.service.CoffeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,14 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CoffeeController {
 
+    @Autowired
+    CoffeeService coffeeService;
+
     @GetMapping(value = "/coffeelover")
     public String getCoffeeLover() {
         return "I like coffee";
     }
 
     @GetMapping(value = "/coffee")
-    public ResponseEntity<String> getCoffee(@RequestParam String name) {
-
-
+    public ResponseEntity<Coffee> Coffee(@RequestParam(defaultValue = "latte") String name) {
+        return new ResponseEntity<>(coffeeService.makeNewCoffee(name), HttpStatus.OK);
     }
 }
